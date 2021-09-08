752a44a4a564b6696e5724141d4cc6b293372ffe
Rafael Alonso <84942957+Salonso1602@users.noreply.github.com>
2021-09-08 15:36:42 -0300

    Initial commit

---
+++ src/Library/Recipe.cs
@@ -0,0 +1,40 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Recipe
+    {
+        public ArrayList steps = new ArrayList();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Step step)
+        {
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+
+        public double GetProductionCost()
+        {
+            double result = 0;
+
+            foreach (Step step in this.steps)
+            {
+                result += step.StepTotal;
+            }
+
+            return result;
+        }
+    }
+}
